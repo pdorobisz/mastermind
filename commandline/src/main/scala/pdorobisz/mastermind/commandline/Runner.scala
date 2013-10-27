00ee777264a9ffd87e54ab6b639fe105e2e1dd71
Piotr Dorobisz <pdorobisz@gmail.com>
2013-10-27 15:50:10 +0100

    implement commandline runner

---
+++ commandline/src/main/scala/pdorobisz/mastermind/commandline/Runner.scala
@@ -0,0 +1,40 @@
+package pdorobisz.mastermind.commandline
+
+import pdorobisz.mastermind.core._
+import pdorobisz.mastermind.core.Incorrect
+import pdorobisz.mastermind.core.IllegalArguments
+import scala.annotation.tailrec
+
+object Runner {
+
+  def main(args: Array[String]) {
+    val config: GameConfig = GameConfig(4, 6, false, GameConfig.NO_GUESS_LIMIT).get
+    val game = Game.init(config)
+    play(game)
+  }
+
+  private def readColors() = readLine("colors: ").toList
+
+  private def printResult(turn: Int, text: String) = println(s"#$turn: $text")
+
+  @tailrec
+  private def play(game: Game): Unit = {
+    val input = readColors()
+    game.guess(input) match {
+      case Finished(turn: Int) =>
+        printResult(turn, "game is already finished!")
+      case GameOver(turn: Int) =>
+        printResult(turn, "GAME OVER")
+      case IllegalArguments(turn: Int) => {
+        printResult(turn, "incorrect input")
+        play(game)
+      }
+      case Incorrect(turn: Int, posOk: Int, colorOk: Int) => {
+        printResult(turn, s"not guessed ($posOk/$colorOk)")
+        play(game)
+      }
+      case Success(turn: Int) =>
+        printResult(turn, "SUCCESS")
+    }
+  }
+}
